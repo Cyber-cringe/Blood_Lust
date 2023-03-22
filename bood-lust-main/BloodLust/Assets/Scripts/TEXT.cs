25c8e623392179d8b269e85795cdca7394d25a48
DiocletianAlfachenko <sashkondrat@mail.ru>
2023-03-22 20:00:08 +0300

    test location

---
+++ bood-lust-main/BloodLust/Assets/Scripts/TEXT.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TEXT : MonoBehaviour
+{
+    public Text t;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void printer()
+    {
+        t.text = "SEX";
+    }
+}
